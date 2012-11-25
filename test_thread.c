2655386340003e1c6efe98a8d463df83961e8bff
Ray Fung <posixfung@163.com>
2012-11-25 22:20:50 +0800

    function: void *two() -> void do_something()

--- test_thread.c
+++ test_thread.c
@@ -1,11 +1,14 @@
 #include "thread_pool.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-#define TASK_NUM 8
+#define TASK_NUM 5
 
-void *two(void *arg) {
+void do_something(void *arg) {
     int n = *(int *)arg;
-    printf("thread %x arg = %d\n", pthread_self(), n);
+    printf("task #%d started\n", n);
+    sleep(rand() % 5 + 1);
+    printf("task #%d finished\n", n);
 }
 
 int main (int argc, char **argv)
@@ -14,13 +17,14 @@ int main (int argc, char **argv)
     int num[TASK_NUM];
     thread_pool_t *pool;
 
-    pool = thread_pool_create(8);
+    pool = thread_pool_create(3);
     for(i = 0; i < TASK_NUM; ++i) {
         num[i] = i + 1;
-        thread_pool_add_task(pool, two, &num[i]);
+        thread_pool_add_task(pool, do_something, &num[i]);
     }
 
     thread_pool_wait_for_done(pool);
+    printf("all tasks are done\n");
     thread_pool_destroy(pool);
 
     return 0;
