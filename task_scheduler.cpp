1cf59025758a49c355b8942bf7824eda3f143473
Ray Fung <posixfung@163.com>
2013-08-13 15:04:05 +0800

    添加、修改成员函数的注释

--- task_scheduler.cpp
+++ task_scheduler.cpp
@@ -59,6 +59,10 @@ void task_scheduler::_run()
     }
 }
 
+/**
+ * 功能：判断一个任务是否已经完成
+ * 返回：假如一个任务已经完成并且尚未checkout，返回true，其它情况返回false
+ */
 bool task_scheduler::is_done(ff_task *task)
 {
     bool done;
@@ -71,6 +75,10 @@ bool task_scheduler::is_done(ff_task *task)
     return done;
 }
 
+/**
+ * 功能：从已完成任务列表中移除一个已完成的任务
+ * 返回：假如指定的任务已完成并且尚未移除，则移除该任务并且返回true，否则返回false
+ */
 bool task_scheduler::checkout(ff_task *task)
 {
     bool done;
@@ -87,7 +95,8 @@ bool task_scheduler::checkout(ff_task *task)
 
 /**
  * 功能：添加一个任务并通知线程池中空闲的线程去执行
- * 注意：不能将已提交的任务再次提交，除非它已经完成
+ * 注意：不能将已提交的任务再次提交，除非它已经完成并且已经从完成列表中移除
+ * 而且，传入的task指针必须指向堆内存，不能指向栈内存
  */
 void task_scheduler::submit(ff_task *task)
 {
@@ -99,7 +108,8 @@ void task_scheduler::submit(ff_task *task)
 }
 
 /**
- * 功能：通知并等待线程池中的线程退出，然后释放线程池占用的所有资源
+ * 功能：通知并等待线程池中的线程退出，然后释放占用的所有资源
+ * 注意：假如有任务未被checkout，那么会自动delete该任务
  */
 task_scheduler::~task_scheduler()
 {
