bbeba1e7da45dda51bea2bcd2eb056c2582e396a
Ray Fung <posixfung@163.com>
2013-08-13 19:23:45 +0800

    计算圆周率Pi

--- main.cpp
+++ main.cpp
@@ -1,48 +1,70 @@
 #include "task_scheduler.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 #define TASK_NUM 5
 
-class demo_task : public ff_task
+class cpi : public ff_task
 {
 public:
-    demo_task(int n)
+    cpi(size_t id, size_t n)
     {
-        this->num = n;
+        this->id = id;
+        this->n = n;
     }
 
-    ~demo_task()
+    ~cpi()
     {
-        printf("task #%d destroyed\n", num);
+        printf("task #%d destroyed\n", id);
         fflush(stdout);
+    }
+
+    double get_partial_pi() const
+    {
+        return this->partial_pi;
     }
 
     void run()
     {
-        printf("task #%d started\n", num);
-        fflush(stdout);
-        sleep(rand() % 5 + 1);
-        printf("task #%d finished\n", num);
-        fflush(stdout);
+        double h;
+        double sum;
+        double x;
+
+        h   = 1.0 / (double) n;
+        sum = 0.0;
+        for (size_t i = id + 1; i <= n; i += TASK_NUM)
+        {
+            x = h * ((double)i - 0.5);
+            sum += f(x);
+        }
+        partial_pi = h * sum;
     }
 
 private:
-    int num;
+    double f(double x)
+    {
+        return (4.0 / (1.0 + x * x));
+    }
+
+private:
+    size_t n;
+    size_t id;
+    double partial_pi;
 };
 
 int main()
 {
-    int i;
     int done_num;
     task_scheduler ts(3);
-    demo_task *task_list[TASK_NUM];
+    cpi *task_list[TASK_NUM];
+    double pi;
+    size_t i;
+    size_t n = 12345678;
 
-    srand(time(0));
     for(i = 0; i < TASK_NUM; ++i)
-        ts.submit(task_list[i] = new demo_task(i));
+        ts.submit(task_list[i] = new cpi(i, n));
 
     done_num = 0;
+    pi = 0.0;
     while(1)
     {
         for(i = 0; i < TASK_NUM; ++i)
@@ -50,14 +72,17 @@ int main()
             if(task_list[i] && ts.is_done(task_list[i]))
             {
                 if(ts.checkout(task_list[i]))
+                {
+                    pi += task_list[i]->get_partial_pi();
                     delete task_list[i];
+                }
                 task_list[i] = NULL;
                 ++done_num;
             }
         }
         if(done_num == TASK_NUM)
         {
-            printf("all tasks are done!\n");
+            printf("pi = %.16f\n", pi);
             fflush(stdout);
             break;
         }
